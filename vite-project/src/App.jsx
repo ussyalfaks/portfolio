a44ff87fdd31972e17304feac2d42055b963eb96
ussyalfaks <usmanalfaki@gmail.com>
2023-08-28 14:15:59 +0000

    portfolio

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,43 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <header>
+        <h3 className="logo">𝑢𝑠𝑚𝑎𝑛</h3>
+        <ul className="nav">
+          <li>
+            <a href=""> Services</a>
+          </li>
+          <li>
+            <a href=""> Works</a>
+          </li>
+          <li>
+            <a href=""> Notes</a>
+          </li>
+          <li>
+            <a href=""> Experince</a>
+          </li>
+        </ul>
+        <h4>+234 8166 280 639</h4>
+      </header>
+
+      <section className="section-one">
+        <div className="sec-one">
+          <div className="greeting-sec">
+            <h1>Hey there, I'm Usman</h1>
+          </div>
+          <div className="what-sec">
+            <h5>i design beatifully simple things, i love what i do.</h5>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
